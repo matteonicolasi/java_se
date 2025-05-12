758a3da6dce45f533dbaf0a6c4d27f2ff488cfa7
matteonicolasi <nicolasi.job@gmail.com>
2025-05-12 15:53:04 +0200

    day 1

---
+++ day_01/ComplementoADueTest.java
@@ -0,0 +1,12 @@
+package day_01;
+
+public class ComplementoADueTest {
+
+	public static void main(String[] args) {
+		int max = Integer.MAX_VALUE;
+		System.out.println("Massimo numero interno rappresentabile in Java: "+max);
+		max = max + 1;
+		System.out.println("Minimo numero interno negativo rappresentabile in Java: "+max);
+	}
+
+}
---
+++ day_01/OperazioniTraDueCaratteri.java
@@ -0,0 +1,18 @@
+package day_01;
+
+public class OperazioniTraDueCaratteri {
+
+	public static void main(String[] args) {
+		char a = 'A';
+		char b = 'B';
+		int somma = a+b;
+		System.out.println("Somma del carattere A + B: "+somma);
+		int differenza = b-a;
+		System.out.println("Differenza del carattere B - A: "+differenza);
+		int prodotto = a*b;
+		System.out.println("Prodotto del carattere A * B: "+prodotto);
+		int quoziente = b/a;
+		System.out.println("Quoziente del carattere B / A: "+quoziente);
+	}
+
+}
